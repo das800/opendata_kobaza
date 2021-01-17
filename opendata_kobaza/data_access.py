a397c7400121ddffaa7457fd73f1edcbb79543a4
Dayan Siddiqui <siddiqui.dayan@gmail.com>
2021-01-17 17:27:40 +0500

    uploaded the two datsets to dynamodb, main page lists datasets from db, links broken though, next commit will fix

---
+++ opendata_kobaza/data_access.py
@@ -0,0 +1,55 @@
+'''
+Dayan Siddiqui
+2021-01-17
+
+purpose: provide abstraction for accessing the datasets and their metavars
+
+reqs: needs aws cli to be installed and configured in the same env to work properly
+'''
+###with aws dynamobd (make sure aws cli is installed and configured)
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
+aws_db_service = 'dynamodb'
+aws_region_id = 'eu-central-1'
+aws_dynamodb_tablename = 'kobaza_ds_metavars'
+
+dynamodbres = boto3.resource(aws_db_service, region_name = aws_region_id)
+dynamodbclient = boto3.client(aws_db_service, region_name = aws_region_id)
+table = dynamodbres.Table(aws_dynamodb_tablename)
+
+def get_all_ds_ids():
+	'''
+	gets all prim keys for ds metavars from the db, TODO never use this as it is stupid to use and to try to get your entire dbs prim keys
+	'''
+	scan_paginator = dynamodbclient.get_paginator('scan')
+
+	scan_paginate_params = {
+		'TableName': aws_dynamodb_tablename,
+		'ProjectionExpression': 'ds_id',
+	}
+	scan_iterator = scan_paginator.paginate(**scan_paginate_params)
+
+	all_ds_ids = []
+	for page in scan_iterator:
+		for item in page['Items']:
+			all_ds_ids.append(item['ds_id']['S'])
+
+	return all_ds_ids
+
+
+def get_names_by_ds_ids(ds_ids):
+	'''
+	takes in a list of ds_ids and return a projection from the db of corresponding names
+	'''
+
+	ds_names_dict = {}
+	for ds_id in ds_ids:
+		query_params = {
+			'ProjectionExpression': "ds_id, #nm",
+			'ExpressionAttributeNames': {"#nm": "name"},
+			'KeyConditionExpression': Key('ds_id').eq(ds_id)
+		}
+		response = table.query(**query_params)
+		ds_names_dict[ds_id] = response['Items'][0]['name']
+	return ds_names_dict
--- opendata_kobaza/main.py
+++ opendata_kobaza/main.py
@@ -13,56 +13,58 @@ app = Flask(__name__)
 
 # import my modules
 import search_kobaza
+import data_access
 
 #softcode paths globally
 ds_metavars_folder = os.path.join(app.root_path, 'datasets', 'metavars')
 ds_folder = os.path.join(app.root_path, 'datasets', 'sets')
 
 ds_metavars_filenames = {
-    'pop': 'SYB63_1_202009_Population, Surface Area and Density_clean.json', 
-    'ind': 'SYB58_35_Index of industrial production_clean.json'
-    }
+	'pop': 'SYB63_1_202009_Population, Surface Area and Density_clean.json', 
+	'ind': 'SYB58_35_Index of industrial production_clean.json'
+	}
 
 
 #main function
 @app.route('/')
 def main():
-    ds_metavars_names = {ds_id: ds_metavars_filenames[ds_id][:-11] for ds_id in ds_metavars_filenames}
-    return render_template('home.html', ds_names_dict = ds_metavars_names)
+	# ds_metavars_names = {ds_id: ds_metavars_filenames[ds_id][:-11] for ds_id in ds_metavars_filenames}
+	ds_names_dict = data_access.get_names_by_ds_ids(data_access.get_all_ds_ids()[:10])
+	return render_template('home.html', ds_names_dict = ds_names_dict)
 
 
 #dataset functions
 @app.route('/datasets/<ds_id>')
 def dataset_page(ds_id):
-    with open(os.path.join(ds_metavars_folder, ds_metavars_filenames[ds_id])) as json_fh:
-        ds_metavars = json.load(json_fh)
-    
-    return render_template('dataset_page.html', meta = ds_metavars)
+	with open(os.path.join(ds_metavars_folder, ds_metavars_filenames[ds_id])) as json_fh:
+		ds_metavars = json.load(json_fh)
+	
+	return render_template('dataset_page.html', meta = ds_metavars)
 
 
 @app.route('/dl_dataset/<dataset_filename>')
 def dl_dataset(dataset_filename):
-    return send_from_directory(ds_folder, dataset_filename)
+	return send_from_directory(ds_folder, dataset_filename)
 
 
 @app.route('/dataset_search', methods = ['GET', 'POST'])
 def search_ds():
-    query = request.form['searchbar']
-    results = []
-    for ds_id in ds_metavars_filenames:
-        with open(os.path.join(ds_metavars_folder, ds_metavars_filenames[ds_id])) as json_fh:
-            ds_sr = search_kobaza.find_q_in_metavarset(json.load(json_fh), query)
-            if len(ds_sr):
-                ds_sr_best_match = ds_sr.get_best_match()
-                results.append((ds_id, search_kobaza.DsDisplaySearchRes(ds_sr.metavar_json['name'], ds_sr.metavar_json['meta-attributes'][ds_sr_best_match[0]][ds_sr_best_match[1]]))) # TODO make ds_id a part of the display sr object putting it with the object as a tuple ad hoc is very bad
+	query = request.form['searchbar']
+	results = []
+	for ds_id in ds_metavars_filenames:
+		with open(os.path.join(ds_metavars_folder, ds_metavars_filenames[ds_id])) as json_fh:
+			ds_sr = search_kobaza.find_q_in_metavarset(json.load(json_fh), query)
+			if len(ds_sr):
+				ds_sr_best_match = ds_sr.get_best_match()
+				results.append((ds_id, search_kobaza.DsDisplaySearchRes(ds_sr.metavar_json['name'], ds_sr.metavar_json['meta-attributes'][ds_sr_best_match[0]][ds_sr_best_match[1]]))) # TODO make ds_id a part of the display sr object putting it with the object as a tuple ad hoc is very bad
 
-    return render_template('search_results.html', query = query, results = results)
+	return render_template('search_results.html', query = query, results = results)
 
 
 #misc
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static', 'img'), 'favicon.ico',mimetype='image/vnd.microsoft.icon')
+	return send_from_directory(os.path.join(app.root_path, 'static', 'img'), 'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 
 
